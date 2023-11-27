590bc3629f889fb020e3e95e85c56dbd7a75c65a
ScarletYue <scarletyue2105@gmail.com>
2023-11-28 03:25:20 +0700

    codeFE

---
+++ formdoan/formdoan/Form1.Designer.cs
@@ -0,0 +1,398 @@
+namespace formdoan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(50, 44);
+            this.listView1.Margin = new System.Windows.Forms.Padding(5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(344, 526);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(700, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(521, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thêm Sản Phẩm Vào Trang Web";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(542, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(863, 651);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thêm sản phẩm";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(458, 433);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(378, 200);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(535, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(282, 37);
+            this.comboBox1.TabIndex = 27;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(752, 311);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(65, 33);
+            this.checkBox4.TabIndex = 26;
+            this.checkBox4.Text = "XL";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(680, 311);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(48, 33);
+            this.checkBox3.TabIndex = 25;
+            this.checkBox3.Text = "L";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(606, 311);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(55, 33);
+            this.checkBox2.TabIndex = 24;
+            this.checkBox2.Text = "M";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(535, 311);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 33);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "S";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 555);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 49);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Thêm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(535, 242);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(282, 37);
+            this.comboBox2.TabIndex = 21;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(680, 385);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 33);
+            this.radioButton2.TabIndex = 19;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Nữ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(535, 385);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 33);
+            this.radioButton1.TabIndex = 18;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nam";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(535, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(282, 34);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(535, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 34);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(346, 450);
+            this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 29);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Mô tả:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(346, 387);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 29);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Giới tính:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(346, 315);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 29);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Size sản phẩm:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(346, 245);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 29);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Màu sản phẩm:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 177);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 29);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Loại sản phẩm:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Giá sản phẩm:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(346, 48);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tên sản phẩm:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hình sản phẩm:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Silver;
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox2.Location = new System.Drawing.Point(43, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(449, 682);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hiển thị xem trước";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 600);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Đăng";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(44, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 355);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1448, 798);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+    #endregion
+
+    private System.Windows.Forms.ListView listView1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label9;
+    private System.Windows.Forms.CheckBox checkBox1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.ComboBox comboBox2;
+    private System.Windows.Forms.RadioButton radioButton2;
+    private System.Windows.Forms.RadioButton radioButton1;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.CheckBox checkBox4;
+    private System.Windows.Forms.CheckBox checkBox3;
+    private System.Windows.Forms.CheckBox checkBox2;
+    private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
+
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -35,36 +35,9 @@ selectCategory2(category: string) {
 
   } 
 
-    products: any[] = [
-      { name: 'T-Shirt', category: 'T-Shirt', cate: 'Nam', price: 100, discountPercent: 20, imageUrl: 'path/to/image1.jpg' },
-      { name: 'T-Shirt', category: 'T-Shirt', cate: 'Nữ', price: 100, discountPercent: 0 , imageUrl: 'path/to/image1.jpg' },
-      { name: 'T-Shirt', category: 'T-Shirt', cate: 'Nam', price: 100, discountPercent: 30, imageUrl: 'path/to/image1.jpg' },
-      { name: 'T-Shirt', category: 'T-Shirt', cate: 'Nữ', price: 100, discountPercent: 0, imageUrl: 'path/to/image1.jpg' },
-      { name: 'Jacket', category: 'Jacket', cate: 'Nam', price: 50, discountPercent: 0, imageUrl: 'path/to/image2.jpg' },
-      { name: 'Jacket', category: 'Jacket', cate: 'Nữ', price: 50, discountPercent: 10, imageUrl: 'path/to/image2.jpg' },
-      { name: 'Jacket', category: 'Jacket', cate: 'Nữ', price: 50, discountPercent: 0, imageUrl: 'path/to/image2.jpg' },
-      { name: 'Jacket', category: 'Jacket', cate: 'Nam', price: 50, discountPercent: 0, imageUrl: 'path/to/image2.jpg' },
-      { name: 'Hoodie', category: 'Hoodie', cate: 'Nam', price: 30, discountPercent: 0, imageUrl: 'path/to/image3.jpg' },
-      { name: 'Hoodie', category: 'Hoodie', cate: 'Nữ', price: 30, discountPercent: 20, imageUrl: 'path/to/image3.jpg' },
-      { name: 'Hoodie', category: 'Hoodie', cate: 'Nam', price: 30, discountPercent: 0, imageUrl: 'path/to/image3.jpg' },
-      { name: 'Hoodie', category: 'Hoodie', cate: 'Nam', price: 30, discountPercent: 0, imageUrl: 'path/to/image3.jpg' },
-      { name: 'Shorts', category: 'Shorts', cate: 'Nữ', price: 30, discountPercent: 0, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Shorts', category: 'Shorts', cate: 'Nam', price: 30, discountPercent: 0, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Shorts', category: 'Shorts', cate: 'Nữ', price: 30, discountPercent: 10, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Shorts', category: 'Shorts', cate: 'Nam', price: 30, discountPercent: 0, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Trousers', category: 'Trousers', cate: 'Nam', price: 30, discountPercent: 10, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Trousers', category: 'Trousers', cate: 'Nữ', price: 30, discountPercent: 0, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Trousers', category: 'Trousers', cate: 'Nam', price: 30, discountPercent: 15, imageUrl: 'path/to/image4.jpg' },
-      { name: 'Trousers', category: 'Trousers', cate: 'Nữ', price: 30, discountPercent: 10, imageUrl: 'path/to/image4.jpg' },
-
-    ];
-
     pageSize: number = 6;
   currentPage: number = 1;
-  totalProducts: number = this.products.length;
   pages: number[] = [];
-  visibleAndFilteredProducts: any[] = [];
-  visibleProducts: any[] = [];
   prevSelectedCategory: string | undefined;
   prevSelectedCate: string | undefined;
   prevSelectedCategoryandcate: string | undefined;
@@ -82,50 +55,23 @@ selectCategory2(category: string) {
 
     }
     this.calculateTotalPages();
-    this.loadVisibleProducts();
   }
 
   calculateTotalPages() {
-    const totalPageCount = Math.ceil(this.filteredProducts.length / this.pageSize);
+    const totalPageCount = Math.ceil( this.pageSize);
     this.pages = Array.from({ length: totalPageCount }, (_, i) => i + 1);
   }
 
   goToPage(page: number) {
     if (page >= 1 && page <= this.pages.length) {
       this.currentPage = page;
-      this.loadVisibleProducts();
     }
   }
 
-  loadVisibleProducts() {
-    const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    this.visibleAndFilteredProducts = this.filteredProducts.slice(startIndex, endIndex)
-     .filter(product => (this.selectedCategory === 'All' || product.category === this.selectedCategory) &&
-                         (this.selectedCate === 'All' || product.cate === this.selectedCate));              
-  }
-
-  get filteredProducts() {
-    let result = this.products;
-
-    if (this.selectedCategory && this.selectedCategory !== 'All') {
-      result = result.filter(product => product.category === this.selectedCategory);
-    }
-
-    if (this.selectedCate && this.selectedCate !== 'All') {
-      result = result.filter(product => product.cate === this.selectedCate);
-    }
-
-    return result;
-  
-  }
 
   ngOnInit() {
   }
   
-  calculateDiscountedPrice(product: any): number {
-    return product.price - (product.price * (product.discountPercent / 100));
-  }
   navigateToForm() {
     this.router.navigate(['/trangchu']);
 
